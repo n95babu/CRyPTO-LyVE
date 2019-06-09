d865f94dd736abf6f86b388af68a820bbbb47115
Nazrul Babu <n95babu@gmail.com>
2019-06-09 19:37:26 -0400

    New API

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const button = document.querySelector('#butt');
 const ul = document.querySelector('#assets'); // List of data will be post
-const API_KEY = '32d342e3d7043f6d8f20c78a3291560f';
-const API = `https://api.coinranking.com/v1/public/coins`
+const newsUl = document.querySelector('#news');
+const news = document.querySelector('#main_News');
+const coinAPI = `https://api.coinranking.com/v1/public/coins`
 
+const newsAPI = `https://min-api.cryptocompare.com/data/v2/news/?lang=EN`
+
+const newsAPIKey = `f09a8461c40803deaf10d1196fbe809d292c6f75e25189abc59b25f0a467fceb`
 
 
 function createNode(element) {
@@ -13,42 +17,67 @@ function append(parent, el) {
 return parent.appendChild(el);
 }
 
-
-
-
-fetch(API)
-  .then((response) => response.json())
-  .then(function(data) {
-  let assets = data.data.coins; 
-  return assets.map(function(coin) { 
+// Coin API  Call
+const getCoinData = async () => {
+  const response = await axios.get(`${coinAPI}`);
+  let assets = response.data.data.coins; 
+  assets.map(function(coin){
     let li = createNode('li');
     let span = createNode('span');
     let img = createNode('img')
-  
-    img.src = coin.iconUrl
-    span.innerHTML = `${coin.price} ${coin.name} `; 
-    append(li, span);
-    append(ul, li);
-    append(li, img);
-    console.log(API)
-  })
-})
 
-.catch(function(error){
-  console.log(error);
-});
+
+
+  img.src = coin.iconUrl
+  span.innerHTML = `${coin.price} ${coin.name} `; 
+  append(li, span);
+  append(ul, li);
+  append(li, img);
+  })
+
+}
+getCoinData()
+
+
+//News API
+
+const GetNewsData = async () => {
+  const response = await axios.get(`${newsAPI}`)
+  let news = response.data.Data
+  news.map(function(coinnews){
+    let li =createNode('li')
+    let span = createNode('span')
+
+    span.innerHTML = `${coinnews.title}`
+    append(li, span);
+    append(newsUl, li)
+  })
+
+
+  console.log(news)
+}
+
+GetNewsData()
+
+
+
+
 
 
 /*Page refresh*/
-setTimeout(function() {
-location.reload();
-}, 10000);
+// setTimeout(function() {
+// location.reload();
+// }, 1000);
 
 
 
 
 
-
+// setInterval(() => {
+// const assets = document.querySelector('#assets');
+// assets.innerHTML = '';
+// getCoinData();
+// }, 10000)
 
 
 
